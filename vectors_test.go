22fa44e8e5ec9a9ebace212fbb1a9a537903aa2f
David Stainton <dstainton415@gmail.com>
2022-08-14 16:43:06 -0400

    Broken test vector test

--- vectors_test.go
+++ vectors_test.go
@@ -17,9 +17,20 @@ func TestVectors(t *testing.T) {
 	err = alicePrivateKey.Unmarshal(alicePrivateKeyBytes)
 	require.NoError(t, err)
 
-	alicePublicKey, err := DerivePublicKey(alicePrivateKey)
+	alicePublicKeyHex := "beb16568f41bff232ab157ebf8e0e80d4dea4446d354fdda9805d73d2ff2f8ba57527ef2c36b33177b82435538ef6598e8fbe2de2827c2cc42591126b96f8536"
+	alicePublicKeyBytes, err := hex.DecodeString(alicePublicKeyHex)
+	alicePublicKey := new(PublicKey)
+	err = alicePublicKey.Unmarshal(alicePublicKeyBytes)
 	require.NoError(t, err)
 
+	/*
+		alicePublicKey, err := DerivePublicKey(alicePrivateKey)
+		require.NoError(t, err)
+
+		alicePublicKeyBytes, err := alicePublicKey.Marshal()
+		require.NoError(t, err)
+		t.Logf("alicePublicKeyBytes %x", alicePublicKeyBytes)
+	*/
 	// Bob
 	bobPrivateKeyHex := "06fc0009fc01ff0201060304fcf501010004020104fd02fff8fefffc0103030100ffff040304ff0102fa0002ff000101fafdfe03ff0400fe01fa00fd0101ff03fe020101030200ff0001"
 	bobPrivateKeyBytes, err := hex.DecodeString(bobPrivateKeyHex)
@@ -28,7 +39,19 @@ func TestVectors(t *testing.T) {
 	err = bobPrivateKey.Unmarshal(bobPrivateKeyBytes)
 	require.NoError(t, err)
 
-	bobPublicKey, err := DerivePublicKey(bobPrivateKey)
+	/*
+		bobPublicKey, err := DerivePublicKey(bobPrivateKey)
+		require.NoError(t, err)
+
+		bobPublicKeyBytes, err := bobPublicKey.Marshal()
+		require.NoError(t, err)
+		t.Logf("bobPublicKeyBytes %x", bobPublicKeyBytes)
+	*/
+
+	bobPublicKeyHex := "5eec9487a6149ed355e59b8a9e3f991f831e43c00d88acba92c4ceb5b294587762529597149994d36da204d21ee72343981f897b9dc36b57c93133461bf4c449"
+	bobPublicKeyBytes, err := hex.DecodeString(bobPublicKeyHex)
+	bobPublicKey := new(PublicKey)
+	err = bobPublicKey.Unmarshal(bobPublicKeyBytes)
 	require.NoError(t, err)
 
 	// NIKE
@@ -39,9 +62,11 @@ func TestVectors(t *testing.T) {
 	require.NoError(t, err)
 
 	bobSharedBytes, err := bobShared.Marshal()
+	//_, err = bobShared.Marshal()
 	require.NoError(t, err)
 
 	aliceSharedBytes, err := aliceShared.Marshal()
+	//_, err = aliceShared.Marshal()
 	require.NoError(t, err)
 
 	require.Equal(t, bobSharedBytes, aliceSharedBytes)
