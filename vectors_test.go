10805b037a42e116b4a6d8e55a78177d9f64357d
David Stainton <dstainton415@gmail.com>
2022-08-16 15:43:19 -0400

    Eradicate commented code, bad bad

--- vectors_test.go
+++ vectors_test.go
@@ -25,14 +25,6 @@ func Test512BitVectors(t *testing.T) {
 	err = alicePublicKey.Unmarshal(alicePublicKeyBytes)
 	require.NoError(t, err)
 
-	/*
-		alicePublicKey, err := DerivePublicKey(alicePrivateKey)
-		require.NoError(t, err)
-
-		alicePublicKeyBytes, err := alicePublicKey.Marshal()
-		require.NoError(t, err)
-		t.Logf("alicePublicKeyBytes %x", alicePublicKeyBytes)
-	*/
 	// Bob
 	bobPrivateKeyHex := "06fc0009fc01ff0201060304fcf501010004020104fd02fff8fefffc0103030100ffff040304ff0102fa0002ff000101fafdfe03ff0400fe01fa00fd0101ff03fe020101030200ff0001"
 	bobPrivateKeyBytes, err := hex.DecodeString(bobPrivateKeyHex)
@@ -40,15 +32,6 @@ func Test512BitVectors(t *testing.T) {
 	bobPrivateKey := new(PrivateKey)
 	err = bobPrivateKey.Unmarshal(bobPrivateKeyBytes)
 	require.NoError(t, err)
-
-	/*
-		bobPublicKey, err := DerivePublicKey(bobPrivateKey)
-		require.NoError(t, err)
-
-		bobPublicKeyBytes, err := bobPublicKey.Marshal()
-		require.NoError(t, err)
-		t.Logf("bobPublicKeyBytes %x", bobPublicKeyBytes)
-	*/
 
 	bobPublicKeyHex := "1e4a6a12ae0218f3eda0213d28e640bf4e39a56847b0374576cb02a18219d7c64ea7e87414ce20eb45566f6cf6243e8fb6f4554e5553e6d4418b4ca609ff6c3a"
 	bobPublicKeyBytes, err := hex.DecodeString(bobPublicKeyHex)
